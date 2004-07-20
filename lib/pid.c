6be504327c7d71ef6862e10eb2663f52e5c3e820
jaredy <>
2004-07-20 00:12:59 +0000

    finish getpidpath(); flesh out common code into readpid(); more cleaning up; ENOENT reporting

--- lib/pid.c
+++ lib/pid.c
@@ -18,13 +18,14 @@
 
 #define PID_MAX INT_MAX
 
+static int readpid(char *, pid_t *);
+
 int
 parsepid(char *s, pid_t *pid)
 {
-	char *p, fil[MAXPATHLEN];
 	struct statfs fst;
-	int ch, isnum;
-	FILE *fp;
+	int isnum;
+	char *p;
 	long l;
 
 	isnum = 1;
@@ -33,28 +34,101 @@ parsepid(char *s, pid_t *pid)
 			isnum = 0;
 			break;
 		}
-
 	if (p == s)
-		return (0);
-
+		goto noent;
 	if (isnum) {
 		l = strtoul(s, NULL, 10);
 		if (l <= PID_MAX) {
 			*pid = (pid_t)l;
 			return (1);
 		} else
-			return (0);
+			goto noent;
 	}
 
 	/* Try /proc/<pid>/status */
 	if (statfs(s, &fst) == -1)
 		return (0);
+	/*
+	 * We can't err() here because they might want
+	 * to consult a corefile instead.
+	 */
 	if (strcmp(fst.f_fstypename, MOUNT_PROCFS) != 0)
 		return (0);
-	(void)snprintf(fil, sizeof(fil), "%s/status", s);
+	if (readpid(s, pid))
+		return (1);
+	else
+		return (0);
+noent:
+	errno = ENOENT;
+	return (0);
+}
+
+char *
+getpidpath(char *s, pid_t *pid)
+{
+	char *p, fil[MAXPATHLEN];
+	struct statfs fst;
+	int isnum;
+	long l;
+
+	/* shutup gcc */
+	l = 0;
+
+	isnum = 1;
+	for (p = s; *p != '\0'; p++)
+		if (!isdigit(*p)) {
+			isnum = 0;
+			break;
+		}
+	if (p == s)
+		goto noent;
+	if (isnum) {
+		if ((l = strtoul(s, NULL, 10)) > PID_MAX)
+			goto noent;
+		snprintf(fil, sizeof(fil), "%s/%s", _PATH_PROC, s);
+		s = fil;
+	}
+	if (statfs(s, &fst) == -1) {
+#if 0
+		if (errno != ENOENT)
+			warn("%s", s);
+#endif
+		return (NULL);
+	}
+	if (strcmp(fst.f_fstypename, MOUNT_PROCFS) != 0) {
+		/* If we built the path, /proc is not mounted. */
+		if (isnum)
+			errx(EX_UNAVAILABLE, "/proc: not mounted");
+		goto noent;
+	}
+	/* Success; copy the pid. */
+	if (isnum) {
+		*pid = (pid_t)l;
+		return (xstrdup(s));
+	} else {
+		/* Else, open and read the pid. */
+		if (readpid(s, pid))
+			return (xstrdup(s));
+		else
+			return (NULL);
+	}
+
+noent:
+	errno = ENOENT;
+	return (NULL);
+}
+
+static int
+readpid(char *s, pid_t *pid)
+{
+	char fil[MAXPATHLEN];
+	FILE *fp;
+	long l;
+	int ch;
+
+	(void)snprintf(fil, sizeof(fil), "%s%s", s, _RELPATH_STATUS);
 	if ((fp = fopen(fil, "r")) == NULL)
 		return (0);
-	/* This worked, use pid from pid field. */
 	while (!isdigit(ch = fgetc(fp)) && ch != EOF)
 		;
 	for (l = 0; isdigit(ch); ch = fgetc(fp))
@@ -64,40 +138,9 @@ parsepid(char *s, pid_t *pid)
 	if (l <= PID_MAX) {
 		*pid = (pid_t)l;
 		return (1);
-	} else
-		return (0);
-}
-
-char *
-getpidpath(char *s)
-{
-	char *p, fil[MAXPATHLEN];
-	struct statfs fst;
-	int isnum;
-
-	isnum = 1;
-	for (p = s; *p != '\0'; p++)
-		if (!isdigit(*p)) {
-			isnum = 0;
-			break;
-		}
-
-	if (p == s)
-		return (NULL);
-	if (isnum) {
-		snprintf(fil, sizeof(fil), "%s/%s", _PATH_PROC, s);
-		s = fil;
 	}
-	if (statfs(s, &fst) == -1) {
-		if (errno != ENOENT)
-			warn("%s", s);
-		return (NULL);
-	}
-	if (strcmp(fst.f_fstypename, MOUNT_PROCFS) != 0) {
-		/* If we built the path, /proc is not mounted. */
-		if (isnum)
-			errx(EX_UNAVAILABLE, "/proc: not mounted");
-		return (NULL);
-	}
-	return (xstrdup(s));
+
+	/* This should never happen. */
+	errno = ENOENT;
+	return (0);
 }
