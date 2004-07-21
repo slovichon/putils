724a7c435afc17ebd41d6208f8c47143e65c89e6
jaredy <>
2004-07-21 02:24:18 +0000

    more fixes; all about to change;

--- ptree/ptree.c
+++ ptree/ptree.c
@@ -1,36 +1,44 @@
 /* $Id$ */
 
+#include <sys/param.h>
 #include <sys/proc.h>
 #include <sys/queue.h>
+#include <sys/sysctl.h>
 
+#include <err.h>
+#include <kvm.h>
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sysexits.h>
 
 #include "putils.h"
 #include "util.h"
-
-struct ptnode {
-	pid_t		 ptn_pid;
-	char 		*ptn_cmd;
-	struct plink	*ptn_children;
-};
 
 struct plink {
 	struct ptnode	*pl_ptn;
 	struct plink	*pl_next;
 };
 
-static		void  buildtree(struct ptnode **);
-static		char *join(char **);
-static __dead	void  usage(void);
+struct ptnode {
+	pid_t		 ptn_pid;
+	char 		*ptn_cmd;
+	struct plink	 ptn_children;
+};
+
+static		char  *join(char **);
+static		void   buildtree(struct ptnode **);
+static		void   dotree(struct ptnode *, char *);
+static		void   freetree(struct ptnode *);
+struct		plink *findpl(struct plink *, pid_t);
+static __dead	void   usage(void);
 
 static int		 showinit = 0;
 
 int
 main(int argc, char *argv[])
 {
-	char buf[_POSIX2_LINE_MAX];
 	struct ptnode *root;
 	int ch;
 
@@ -52,49 +60,52 @@ main(int argc, char *argv[])
 			dotree(root, *argv);
 	else
 		dotree(root, NULL);
-	freetree(&root);
+	freetree(root);
 	exit(EXIT_SUCCESS);
 }
 
 static void
 buildtree(struct ptnode **t)
 {
-	struct plink unacc, *pl, *plchild, *pl;
+	char **argv, buf[_POSIX2_LINE_MAX];
+	struct plink unacc, *pl, *plchild;
 	struct ptnode *ptn, *ptnchild;
 	struct kinfo_proc *kip;
 	struct proc *child;
-	char **argv;
+	int i, pcnt;
 	kvm_t *kd;
-	int i;
 
 	if ((kd = kvm_openfiles((char *)NULL, (char *)NULL, (char *)NULL,
-	    0, buf)) == NULL)
+	     KVM_NO_FILES, buf)) == NULL)
 		errx(EX_OSERR, "kvm_openfiles: %s", buf);
-	kip = kvm_getproc(kd, KERN_PROC_KTHREAD, 0, &pcnt);
+	kip = kvm_getprocs(kd, KERN_PROC_KTHREAD, 0, &pcnt);
 	if (kip == NULL || pcnt == 0)
-		errx(EX_OSERR, "kvm_getproc2: %s", kvm_geterr(kd));
+		errx(EX_OSERR, "kvm_getprocs: %s", kvm_geterr(kd));
 	unacc.pl_next = NULL;
 	for (i = 0; i < pcnt; i++, kip++) {
 		if ((argv = kvm_getargv(kd, kip, 0)) == NULL)
-			err(EX_OSERR, "kvm_getargv2: ", kvm_geterr(kde));
-		if ((pl = findpl(&unacc, kip->kp_proc.p_pid)) == NULL)
+			err(EX_OSERR, "kvm_getargv: %s", kvm_geterr(kd));
+		if ((pl = findpl(&unacc, kip->kp_proc.p_pid)) == NULL) {
 			if ((ptn = malloc(sizeof(*ptn))) == NULL)
 				err(EX_OSERR, "malloc");
 			/* Add to list of unaccounted nodes. */
-			if (pl = malloc(sizeof(*pl)) == NULL)
+			if ((pl = malloc(sizeof(*pl))) == NULL)
 				err(EX_OSERR, "malloc");
-			pl->pl_next = unacc.next;
+			pl->pl_next = unacc.pl_next;
 			pl->pl_ptn = ptn;
-			unacc.next = pl;
+			unacc.pl_next = pl;
 		} else {
 			/*
 			 * A node for this process has already
 			 * been allocated, so use it.
 			 */
-			ptn = pl->pl->next->pl_ptn;
+			ptn = pl->pl_next->pl_ptn;
 		}
-		plchildren = NULL;
-		LIST_FOREACH(child, &kip->kp_proc.p_children, proc) {
+		ptn->ptn_children.pl_next = NULL;
+		ptn->ptn_cmd = join(argv);
+		ptn->ptn_pid = kip->kp_proc.p_pid;
+		for (child = kip->kp_proc.p_children.lh_first;
+		     child != NULL; child = child->p_sibling.le_next) {
 			if ((pl = findpl(&unacc, child->p_pid)) == NULL) {
 				/* Child not found; allocate new. */
 				if ((plchild = malloc(sizeof(*pl))) == NULL)
@@ -104,7 +115,7 @@ buildtree(struct ptnode **t)
 					err(EX_OSERR, "malloc");
 				ptnchild->ptn_cmd = NULL;
 				ptnchild->ptn_pid = child->p_pid;
-				ptnchild->ptn_children = NULL;
+				ptnchild->ptn_children.pl_next = NULL;
 				plchild->pl_ptn = ptnchild;
 			} else {
 				/*
@@ -114,34 +125,33 @@ buildtree(struct ptnode **t)
 				plchild = pl->pl_next;
 				pl->pl_next = plchild->pl_next;
 			}
-			plchild->pl_next = plchildren;
-			plchildren = plchild;
+			plchild->pl_next = ptn->ptn_children.pl_next;
+			ptn->ptn_children.pl_next = plchild;
 		}
-		ptn->ptn_children = plchildren;
-		ptn->ptn_cmd = join(argv);
-		ptn->ptn_pid = kip->p_pid;
 	}
 	(void)kvm_close(kd);
 }
 
 struct plink *
-findpl(struct plink *base, pid_t pid)
+findpl(struct plink *pl, pid_t pid)
 {
-	struct plink *pl, *plchild;
-	struct ptnode *ptn;
+	struct plink *plchild;
 
-	pl = NULL;
-	for (; base != NULL; base = base->pl_next) {
-		ptn = pl->pl_ptn;
-		findpl();
+	for (; pl->pl_next != NULL; pl = pl->pl_next) {
+		if (pl->pl_next->pl_ptn->ptn_pid == pid)
+			return (pl);
+		/* Recursively search grandchildren. */
+		if ((plchild = findpl(&pl->pl_next->pl_ptn->ptn_children,
+		     pid)) != NULL)
+			return (plchild);
 	}
-	return pl;
+	return (NULL);
 }
 
 char *
 join(char **s)
 {
-	size_t siz;
+	size_t siz = 0;
 	char *p, **t;
 
 	for (t = s; *t != NULL; t++)
@@ -156,7 +166,7 @@ join(char **s)
 	p[0] = '\0';
 	for (t = s; *t != NULL; t++)
 		strlcat(p, *t, siz);
-	return p;
+	return (p);
 }
 
 static void
@@ -165,12 +175,12 @@ freetree(struct ptnode *ptn)
 	struct plink *pl, *next;
 
 	free(ptn->ptn_cmd);
-	for (pl = ptn->ptn_child; *pl != NULL; pl = next) {
+	for (pl = ptn->ptn_children.pl_next; pl != NULL; pl = next) {
 		next = pl->pl_next;
 		freetree(pl->pl_ptn);
 		free(pl);
 	}
-	free(p);
+	free(ptn);
 }
 
 static void
