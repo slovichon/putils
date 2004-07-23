564a8ac453a96f04831af54b531d3b7bb2de3f49
jaredy <>
2004-07-23 02:59:38 +0000

    add join()

--- lib/util.c
+++ lib/util.c
@@ -4,6 +4,9 @@
 #include <errno.h>
 #include <stdarg.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sysexits.h>
 
 #include "util.h"
 
@@ -26,4 +29,27 @@ xwarn(char *fmt, ...)
 			vwarnx(fmt, ap);
 		va_end(ap);
 	}
+}
+
+char *
+join(char **s)
+{
+	size_t siz = 0;
+	char *p, **t;
+
+	for (t = s; *t != NULL; t++)
+		/*
+		 * Add 1+strlen for each, which takes care
+		 * of NUL for the last argument.
+		 */
+		siz += strlen(*t) + 1;
+
+	if ((p = malloc(siz)) == NULL)
+		err(EX_OSERR, "malloc");
+	p[0] = '\0';
+	for (t = s; *t != NULL; t++) {
+		strlcat(p, *t, siz);
+		strlcat(p, " ", siz);
+	}
+	return (p);
 }
