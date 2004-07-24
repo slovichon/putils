cef402ddc10c5217173da4825df263065c073187
jaredy <>
2004-07-24 21:32:14 +0000

    fix proto

--- lib/lbuf.h
+++ lib/lbuf.h
@@ -9,4 +9,4 @@ void  lbuf_init(struct lbuf **);
 void  lbuf_append(struct lbuf *, char);
 char *lbuf_get(struct lbuf *);
 void  lbuf_free(struct lbuf **);
-void  lbuf_reset(struct lbuf *lb);
+void  lbuf_reset(struct lbuf *);
