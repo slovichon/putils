cdd534a0fe980f7a2c1a0ac18f05f885d468bbad
jaredy <>
2004-08-09 02:42:38 +0000

    remove test files on clean; enable tests

--- mk/lib.mk
+++ mk/lib.mk
@@ -12,10 +12,13 @@ ${LIB}: ${OBJS}
 	${RANLIB} ${LIB}
 
 clean:
-	rm -rf ${LIB} ${OBJS}
+	rm -rf ${LIB} ${OBJS} ${TEST} ${TEST}.o
 
 obj:
 	mkdir obj
 
 depend:
 	mkdep ${CFLAGS} ${OBJS:.o=.c:C/^/${.CURDIR}\//}
+
+test: ${TEST}.o ${OBJS}
+	${CC} ${LIBS} -o ${TEST} ${OBJS} ${TEST}.o
