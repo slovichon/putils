31f08758f672476b31d35d4569a3ad9644ca6b66
jaredy <>
2004-08-09 02:43:58 +0000

    spacing; add struct stat member to elfsymtab; change return value for est_open()

--- lib/symtab.h
+++ lib/symtab.h
@@ -1,13 +1,17 @@
 /* $Id$ */
 
+#include <sys/types.h>
+#include <sys/stat.h>
+
 #include <elf_abi.h>
 
 struct elfsymtab {
-	FILE	*est_fp;
-	Elf_Ehdr est_ehdr;
-	Elf_Shdr est_shdr;
+	FILE		*est_fp;
+	struct stat	 est_st;
+	Elf_Ehdr	 est_ehdr;
+	Elf_Shdr	*est_shdrs;
 };
 
-int	 est_open(struct elfsymtab *, char *);
-char	*est_symgetname(struct elfsymtab *, unsigned long);
-void	 est_close(struct elfsymtab *);
+struct elfsymtab	*est_open(char *);
+char			*est_symgetname(struct elfsymtab *, unsigned long);
+void			 est_close(struct elfsymtab *);
