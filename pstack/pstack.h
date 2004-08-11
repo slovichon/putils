dd0c3d76687c25155ef38925362bae7cab771efe
jaredy <>
2004-08-11 00:19:29 +0000

    proto changed

--- pstack/pstack.h
+++ pstack/pstack.h
@@ -1,8 +1,9 @@
 /* $Id$ */
 
+#include <sys/types.h>
 #include <sys/param.h>
 #include <sys/sysctl.h>
 
 #include <kvm.h>
 
-unsigned long getsp(kvm_t *, struct kinfo_proc2 *);
+char *getsp(kvm_t *, struct kinfo_proc2 *, unsigned long *);
