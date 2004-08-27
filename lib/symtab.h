a201b65b8082682c271c40628b0fd8ab63406717
jaredy <>
2004-08-27 08:14:01 +0000

    spacing; move symtab lookup variables here so they are only computed once

--- lib/symtab.h
+++ lib/symtab.h
@@ -14,11 +14,19 @@ struct symtab {
 	} st_hdr;
 	union symtab_data {
 		struct symtab_data_elf {
-			Elf_Shdr	 *elf_shdrs;
+			Elf_Shdr	*elf_shdrs;
+			char		*elf_shnams;
+			char		*elf_symnams;
+			int		 elf_nsyms;
+			off_t		 elf_stpos;
 		} dat_elf;
 	} st_data;
 #define st_ehdr		st_hdr.hdr_elf
-#define st_shdrs	st_data.dat_elf.elf_shdrs
+#define st_eshdrs	st_data.dat_elf.elf_shdrs
+#define st_eshnams	st_data.dat_elf.elf_shnams
+#define st_esymnams	st_data.dat_elf.elf_symnams
+#define st_ensyms	st_data.dat_elf.elf_nsyms
+#define st_estpos	st_data.dat_elf.elf_stpos
 };
 
 struct symtab	*symtab_open(char *);
