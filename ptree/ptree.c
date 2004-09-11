774e673591676c35d8550579a0592220fafc2e80
jaredy <>
2004-09-11 22:40:32 +0000

    drop recursion, use a stack instead; properly implement -a; properly print ascendents

--- ptree/ptree.c
+++ ptree/ptree.c
@@ -6,6 +6,7 @@
 #include <sys/sysctl.h>
 
 #include <err.h>
+#include <errno.h>
 #include <kvm.h>
 #include <limits.h>
 #include <stdio.h>
@@ -35,7 +36,6 @@ struct ptnode {
 
 static		void   buildtree(struct ptnode **);
 static		void   doproc(struct ptnode *, char *);
-static		void   findtree(struct ptnode *, pid_t, int);
 static		void   freetree(struct ptnode *);
 struct		plink *findpl(struct plink *, pid_t);
 static __dead	void   usage(void);
@@ -107,19 +107,21 @@ buildtree(struct ptnode **root)
 			/* Found parent; add there. */
 			child->pl_next = pl->pl_ptn->ptn_children.pl_next;
 			pl->pl_ptn->ptn_children.pl_next = child;
+			child->pl_ptn->ptn_parent = pl->pl_ptn;
 		}
-		if ((argv = kvm_getargv2(kd, kip, 0)) == NULL)
-			child->pl_ptn->ptn_cmd = strdup(kip->p_comm);
+		if ((argv = kvm_getargv2(kd, kip, 0)) == NULL) {
 			/* err(EX_OSERR, "kvm_getargv: %s", kvm_geterr(kd)); */
-		else
+			if ((child->pl_ptn->ptn_cmd = strdup(kip->p_comm)) == NULL)
+				err(EX_OSERR, NULL);
+		} else
 			child->pl_ptn->ptn_cmd = join(argv);
 		child->pl_ptn->ptn_pid = kip->p_pid;
 		child->pl_ptn->ptn_ppid = kip->p_ppid;
-		child->pl_ptn->ptn_pgid = kip->p_pgid;
+		child->pl_ptn->ptn_pgid = kip->p__pgid;
 		/* Place in hash queue. */
 		if ((pl = malloc(sizeof(*pl))) == NULL)
 			err(EX_OSERR, "malloc");
-		pos = hash(kip->p_pid, pcnt * 2);
+		pos = hash(kip->p_pid, phashtabsiz);
 		pl->pl_next = phashtab[pos];
 		phashtab[pos] = pl;
 		pl->pl_ptn = child->pl_ptn;
@@ -138,14 +140,19 @@ buildtree(struct ptnode **root)
 		/* Remove child. */
 		child = unacc.pl_next;
 		unacc.pl_next = child->pl_next;
-		/* Scan both unaccounted-for nodes and nodes under root. */
-		if ((pl = findpl(&unacc, child->pl_ptn->ptn_ppid)) == NULL)
-			if ((pl = findpl(&rootpl, child->pl_ptn->ptn_ppid)) ==
-			    NULL)
-				errx(EX_SOFTWARE, "improper tree building (%d)",
-				    child->pl_ptn->ptn_pid);
+		/*
+		 * Scan both unaccounted-for nodes and nodes under root
+		 * for parent.
+		 */
+		if ((pl = findpl(&unacc, child->pl_ptn->ptn_ppid)) ==
+		    NULL)
+			if ((pl = findpl(&rootpl,
+			     child->pl_ptn->ptn_ppid)) == NULL)
+				errx(EX_SOFTWARE, "invalid tree structure "
+				    "(pid %d)", child->pl_ptn->ptn_pid);
 		child->pl_next = pl->pl_next->pl_ptn->ptn_children.pl_next;
 		pl->pl_next->pl_ptn->ptn_children.pl_next = child;
+		child->pl_ptn->ptn_parent = pl->pl_next->pl_ptn;
 	}
 }
 
@@ -206,37 +213,24 @@ freetree(struct ptnode *ptn)
 	}
 }
 
-static struct plink *
-pldup(struct plink *pl)
-{
-	struct plink *dup;
-
-	if ((dup = malloc(sizeof(*dup))) == NULL)
-		err(EX_OSERRR, NULL);
-	dup->pl_next = NULL;
-	dup->pl_ptn = pl->pl_ptn;
-	return (dup);
-}
-
 static void
 doproc(struct ptnode *root, char *s)
 {
-	struct plink *anc, *desc, *pl, *target, *next, *dup;
+	struct plink *anc, *desc, *pl, *next, *dup;
+	struct ptnode *target, *anctn;
 	pid_t pid;
 
 	if (!parsepid(s, &pid)) {
 		xwarn("cannot examine %s", s);
 		return;
 	}
-	findtree(root, pid, 0);
 
 	/* Find process. */
-	if (root->ptn_pid == pid)
-		found = 1;
-	for (pl = root->ptn_children.pl_next; pl != NULL;
-	     pl = pl->pl_next)
-		findtree(pl->pl_ptn, pid, found);
-	for (pl = root; pl != NULL; pl = pl->pl_next) {
+	if ((pl = malloc(sizeof(*pl))) == NULL)
+		err(EX_OSERR, NULL);
+	pl->pl_next = NULL;
+	pl->pl_ptn = root;
+	for (; pl != NULL; pl = pl->pl_next) {
 		if (pl->pl_ptn->ptn_pid == pid)
 			break;
 		for (next = pl->pl_ptn->ptn_children.pl_next;
@@ -249,70 +243,66 @@ doproc(struct ptnode *root, char *s)
 		}
 	}
 	if (pl == NULL) {
-		errno = ENOENT;
+		errno = ESRCH;
 		warn("cannot examine %d", pid);
 		return;
 	}
+	target = pl->pl_ptn;
 
 	/* Collect and dump ancestors. */
 	anc = NULL;
-	while (showinit ? ptn->ptn_pid != 0 :
-	    ptn->ptn_pgid == ptn->ptn_pid) {
+	anctn = target->ptn_parent;
+	pl->pl_ptn = pl->pl_ptn->ptn_parent;
+	while (pl->pl_ptn != NULL /* Special case for pid 0. */ &&
+	    (showinit ? pl->pl_ptn->ptn_pid != 0 :
+	     pl->pl_ptn->ptn_pgid != pl->pl_ptn->ptn_pid)) {
 		if ((pl = malloc(sizeof(*pl))) == NULL)
 			err(EX_OSERR, NULL);
 		pl->pl_next = anc;
-		pl->pl_ptn = target;
+		pl->pl_ptn = anctn;
 		anc = pl;
-		target = target->ptn_parent;
+		anctn = anctn->ptn_parent;
 	}
 
 	level = 0;
 	for (level = 0; anc != NULL; anc = next, level++) {
-		(void)printf("%*s%d %s\n", level * 2, "", anc->ptn_pid,
-		    anc->ptn_cmd);
+		(void)printf("%*s%d %s\n", level * 2, "",
+		    anc->pl_ptn->ptn_pid, anc->pl_ptn->ptn_cmd);
 		next = anc->pl_next;
 		free(anc);
 	}
 
 	/* Collect and dump descendents. */
-	desc = ;
+	if ((desc = malloc(sizeof(*desc))) == NULL)
+		err(EX_OSERR, NULL);
+	desc->pl_next = NULL;
+	desc->pl_ptn = target;
+	desc->pl_last = 0;
 	for (; desc != NULL; desc = next) {
-		(void)printf("%*s%d %s\n", level * 2, "", desc->ptn_pid,
-		    desc->ptn_cmd);
-		if (desc->pl_last)
+		while (desc->pl_last > 0) {
 			level--;
+			desc->pl_last--;
+		}
+		(void)printf("%*s%d %s\n", level * 2, "",
+		    desc->pl_ptn->ptn_pid, desc->pl_ptn->ptn_cmd);
+		if (desc->pl_ptn->ptn_children.pl_next != NULL) {
+			level++;
+			if (desc->pl_next != NULL)
+				desc->pl_next->pl_last++;
+		}
 		for (pl = desc->pl_ptn->ptn_children.pl_next;
 		     pl != NULL;
 		     pl = pl->pl_next) {
 			if ((next = malloc(sizeof(*next))) == NULL)
 				err(EX_OSERR, NULL);
-			next = desc->pl_next;
+			next->pl_last = 0;
+			next->pl_ptn = pl->pl_ptn;
+			next->pl_next = desc->pl_next;
 			desc->pl_next = next;
 		}
-		if (desc->pl_ptn->ptn_children.pl_next != NULL)
-			level++;
 		next = desc->pl_next;
 		free(desc);
 	}
-}
-
-static void
-findtree(struct ptnode *root, pid_t pid, int found)
-{
-	struct plink *pl;
-
-	if (root->ptn_pid == pid)
-		found = 1;
-	if (found) {
-		(void)printf("%*s%d %s\n", level * 2, "", root->ptn_pid,
-		    root->ptn_cmd);
-		level++;
-	}
-	for (pl = root->ptn_children.pl_next; pl != NULL;
-	     pl = pl->pl_next)
-		findtree(pl->pl_ptn, pid, found);
-	if (found)
-		level--;
 }
 
 static __dead void
