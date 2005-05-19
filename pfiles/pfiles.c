3f881f1698ef5edfe73f2ce0ca03994c83deecac
yanovich <>
2005-05-19 01:56:21 +0000

    more initial work: restructure more sanely

--- pfiles/pfiles.c
+++ pfiles/pfiles.c
@@ -5,6 +5,7 @@
 #include <sys/filedesc.h>
 #include <sys/resource.h>
 #include <sys/stat.h>
+#include <sys/vnode.h>
 
 #define _KERNEL /* XXX */
 #include <sys/file.h>
@@ -19,9 +20,12 @@
 #include <stdlib.h>
 #include <sysexits.h>
 
+#include "pfiles.h"
+
 #define PID_MAX	INT_MAX
 
 int		 pfiles(char *);
+void		 pfile(int, struct file *, char *);
 __dead void	 usage(void);
 
 kvm_t		*kd = NULL;
@@ -58,13 +62,13 @@ main(int argc, char *argv[])
 int
 pfiles(char *s)
 {
-	char **argv, *p, *fileflags;
-	int pcnt, saw, i, j, pr_rdwr;
+	char **argv, *fileflags;
 	struct file fil, *fp, **fpp;
 	struct kinfo_proc2 *kp;
 	struct filedesc fdt;
 	const char *errstr;
 	struct plimit pl;
+	int pcnt, j;
 	size_t siz;
 	int error;
 	pid_t pid;
@@ -119,83 +123,97 @@ pfiles(char *s)
 		if (kvm_read(kd, (u_long)fp, &fil, sizeof(fil)) !=
 		    sizeof(fil))
 			err(EX_OSERR, "kvm_read: %s", kvm_geterr(kd));
-		switch (fil.f_type) {
-		case S_IFIFO:	p = "S_IFIFO";  break;
-		case S_IFCHR:	p = "S_IFCHR";  break;
-		case S_IFDIR:	p = "S_IFDIR";  break;
-		case S_IFBLK:	p = "S_IFBLK";  break;
-		case S_IFREG:	p = "S_IFREG";  break;
-		case S_IFLNK:	p = "S_IFLNK";  break;
-		case S_IFSOCK:	p = "S_IFSOCK"; break;
-		case S_IFWHT:	p = "S_IFWHT";  break;
-		default:	p = "???";	break;
-		}
-		(void)printf("%4d: %s ", j, p);
-		(void)printf("mode: 0%03o ", 0);
-		(void)printf("dev: %d,%d ", 0,0);
-		(void)printf("ino: %d ", 0);
-		(void)printf("uid: %d ", 0);
-		(void)printf("gid: %d ", 0);
-		(void)printf("rdev: %d,%d\n	", 0,0);
-		saw = 0;
-		pr_rdwr = 0;
-		for (i = 0; fil.f_flag >> i; i++) {
-			switch (fil.f_flag & (1 << i)) {
-			case FREAD:
-			case FWRITE:
-				if (pr_rdwr) {
-					p = NULL;
-					break;
-				}
-				pr_rdwr = 1;
-				if (fil.f_flag & (FREAD | FWRITE))
-					p = "O_RDWR";
-				else if (fil.f_flag & FREAD)
-					p = "O_RDONLY";
-				else if (fil.f_flag & FWRITE)
-					p = "O_WRONLY";
-				break;
-			case FNONBLOCK:	p = "O_NONBLOCK"; break;
-			case FAPPEND:	p = "O_APPEND";	break;
-			case FHASLOCK:
-				p = "O_SHLOCK";
-				p = "O_EXLOCK";
-				break;
-			case FASYNC:	p = "O_ASYNC";	break;
-			case FFSYNC:	p = "O_SYNC";	break;
-			default:	p = NULL;	break;
-			}
-			if (p == NULL)
-				saw = 0;
-			else {
-				(void)printf("%s%s", saw ? "|" : "", p);
-				saw = 1;
-			}
-		}
-		if (fileflags[j])
-			(void)printf(" ");
-		saw = 0;
-		for (i = 0; fileflags[j] >> i; i++) {
-			switch (fileflags[j] & (1 << i)) {
-			case UF_EXCLOSE:
-				p = "FD_CLOEXEC";
-				break;
-			default:
-				p = NULL;
-				break;
-			}
-			if (p == NULL)
-				saw = 0;
-			else {
-				(void)printf("%s%s", saw ? "|" : "", p);
-				saw = 1;
-			}
-		}
-		(void)printf("\n");
+		pfile(j, &fil, fileflags);
 	}
 done:
 	free(fileflags);
 	return (error);
+}
+
+void
+pfile(int j, struct file *fil, char *fileflags)
+{
+	int i, saw, pr_rdwr;
+	struct vnode vn;
+	const char *p = "???";
+
+	if (fil->f_type == DTYPE_VNODE) {
+		if (kvm_read(kd, (u_long)fil->f_data, &vn,
+		    sizeof(vn)) != sizeof(vn))
+			err(EX_OSERR, "kvm_read: %s", kvm_geterr(kd));
+		switch (vn.v_type) {
+		case VFIFO:	p = "S_IFIFO";  break;
+		case VCHR:	p = "S_IFCHR";  break;
+		case VDIR:	p = "S_IFDIR";  break;
+		case VBLK:	p = "S_IFBLK";  break;
+		case VREG:	p = "S_IFREG";  break;
+		case VLNK:	p = "S_IFLNK";  break;
+		case VSOCK:	p = "S_IFSOCK"; break;
+	//	case VWHT:	p = "S_IFWHT";  break;
+		}
+	}
+	(void)printf("%4d: %s ", j, p);
+	(void)printf("mode:0%03o ", 0);
+	(void)printf("dev:%d,%d ", 0,0);
+	(void)printf("ino:%d ", 0);
+	(void)printf("uid:%d ", 0);
+	(void)printf("gid:%d ", 0);
+	(void)printf("rdev:%d,%d\n      ", 0,0);
+	saw = 0;
+	pr_rdwr = 0;
+	for (i = 0; fil->f_flag >> i; i++) {
+		switch (fil->f_flag & (1 << i)) {
+		case FREAD:
+		case FWRITE:
+			if (pr_rdwr) {
+				p = NULL;
+				break;
+			}
+			pr_rdwr = 1;
+			if (fil->f_flag & (FREAD | FWRITE))
+				p = "O_RDWR";
+			else if (fil->f_flag & FREAD)
+				p = "O_RDONLY";
+			else if (fil->f_flag & FWRITE)
+				p = "O_WRONLY";
+			break;
+		case FNONBLOCK:	p = "O_NONBLOCK"; break;
+		case FAPPEND:	p = "O_APPEND";	break;
+		case FHASLOCK:
+			p = "O_SHLOCK";
+			p = "O_EXLOCK";
+			break;
+		case FASYNC:	p = "O_ASYNC";	break;
+		case FFSYNC:	p = "O_SYNC";	break;
+		default:	p = NULL;	break;
+		}
+		if (p == NULL)
+			saw = 0;
+		else {
+			(void)printf("%s%s", saw ? "|" : "", p);
+			saw = 1;
+		}
+	}
+	if (fileflags[j])
+		(void)printf(" ");
+	saw = 0;
+	for (i = 0; fileflags[j] >> i; i++) {
+		switch (fileflags[j] & (1 << i)) {
+		case UF_EXCLOSE:
+			p = "FD_CLOEXEC";
+			break;
+		default:
+			p = NULL;
+			break;
+		}
+		if (p == NULL)
+			saw = 0;
+		else {
+			(void)printf("%s%s", saw ? "|" : "", p);
+			saw = 1;
+		}
+	}
+	(void)printf("\n");
 }
 
 void
